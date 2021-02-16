fa805c71904fc23fc7940f4f71d4cca0c457d949
Gökhan Arkan <gokhanarkan@gmail.com>
2021-02-16 18:35:05 +0300

    newsletter server logic added

--- components/PostNavigation.tsx
+++ components/PostNavigation.tsx
@@ -19,9 +19,9 @@ const PostNavigation = ({next="", previous=""}) => {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
                 ></path>
               </svg>
@@ -46,9 +46,9 @@ const PostNavigation = ({next="", previous=""}) => {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M13 5l7 7-7 7M5 5l7 7-7 7"
                 ></path>
               </svg>
