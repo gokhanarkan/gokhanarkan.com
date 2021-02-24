2e13bc93e659f5a1bf8a5d708f9d0f8a1d776975
Gökhan Arkan <gokhanarkan@gmail.com>
2021-02-24 20:33:56 +0300

    Only published posts will be written in sitemap

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -9,13 +9,15 @@ export const getAllPosts = async () => {
 const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
         ${posts
-          .map(({ slug, date }) => {
-            return `
+          .map(({ slug, date, published }) => {
+            if (published) { // Only published blog posts will be written in sitemap
+              return `
                     <url>
                         <loc>${`${process.env.BASE_URL}/blog/${slug}`}</loc>
                         <lastmod>${new Date(date)}</lastmod>
                     </url>
                 `;
+            }
           })
           .join("")}
           <url>
