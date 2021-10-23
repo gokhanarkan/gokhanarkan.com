65ed86f08cb00457aeedb6ea1f2a2041d54d8398
Gokhan Arkan <gokhanarkan@gmail.com>
2021-10-23 08:27:07 +0100

    Api route is completed

--- pages/api/barclays/check-phone-number.ts
+++ pages/api/barclays/check-phone-number.ts
@@ -1,4 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from "next";
 import { redis } from "../../../lib/redis";
+
+const sanitizeNumber = (num: string) => {
+  if (num.startsWith("+")) num = num.substring(3);
+  num = num.replace(/[^0-9]/g, "");
+  if (!Boolean(num)) return false;
+  return num;
+};
 
 const checkNumber = async (num: string) => {
   try {
@@ -9,15 +17,17 @@ const checkNumber = async (num: string) => {
   }
 };
 
-const sanitizeNumber = (num: string) => {
-  return num.replace(/^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g, "");
-};
-
-export default async function handler(req, res) {
-  const number = req.query.number;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const number = req.query.number as string;
   if (!number)
     return res.status(422).json({ error: "Number field is missing." });
-  const found = await checkNumber(number);
+  const sanitized = sanitizeNumber(number);
+  if (!sanitized)
+    return res.status(422).json({ error: "Number is not applicable." });
+  const found = await checkNumber(sanitized);
   res.status(200);
   res.json({
     found,
